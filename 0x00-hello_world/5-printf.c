c8b019649418cb3f52a7bec092be1573b552850f
charlesBAAHtoday <charlesbaahtoday@gmail.com>
2023-09-07 19:58:07 -0700

    A C program that prints exactly 'with proper grammar..'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function is where the work starts - just being funny!
+ *
+ * Return: 0 when no errors are encountered or successful
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
